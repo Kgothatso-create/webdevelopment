69a01bc8c4ed7b278a7fed8917e59b448cdcf6b0
Kgothatso-create <128015599+Kgothatso-create@users.noreply.github.com>
2023-08-10 06:15:40 +0200

    final commit

--- single player dice game/script.js
+++ single player dice game/script.js
@@ -1,3 +1,4 @@
+// An array of dice image filenames
 let images = [
     "1.png",
     "2.png",
@@ -7,30 +8,40 @@ let images = [
     "6.png",
 ];
 
+// Select all the img elements on the page (the dice images)
 let dice = document.querySelectorAll("img");
 
-function roll(){
+// Function that simulates rolling the dice
+function roll() {
+    // Add a "shake" class to each dice image to create a shaking animation
     dice.forEach(function(die){
         die.classList.add("shake");
     });
 
+    // After a delay of 1000ms (1 second), execute the following code
     setTimeout(function(){
+        // Remove the "shake" class from each dice image to stop the shaking animation
         dice.forEach(function(die){
             die.classList.remove("shake");
         });
 
-        let dieOneValue = Math.floor(Math.random()*6);
-        let dieTwoValue = Math.floor(Math.random()*6);
+        // Generate random values for the two dice
+        let dieOneValue = Math.floor(Math.random() * 6);
+        let dieTwoValue = Math.floor(Math.random() * 6);
 
+        // Display the random values in the console
         console.log(dieOneValue, dieTwoValue);
 
+        // Update the src attribute of the dice images to show the rolled values
         document.querySelector("#die-1").setAttribute("src", "images/" + images[dieOneValue]);
         document.querySelector("#die-2").setAttribute("src", "images/" + images[dieTwoValue]);
 
+        // Calculate the total of the two dice values and display it in the "total" paragraph
         document.querySelector("#total").innerHTML = "Your roll is " + ((dieOneValue + 1) + (dieTwoValue + 1));
     },
-    1000
+    1000 // Delay of 1000ms (1 second)
     );
 }
 
+// Initial roll when the script is first loaded
 roll();
